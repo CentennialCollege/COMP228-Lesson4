4ca46a7eb073962be2bdf412f1e120e983126ba8
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-09-29 19:23:07 -0400

    Added Person Class

--- src/Program.java
+++ src/Program.java
@@ -5,23 +5,21 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		int myAge;
-		boolean isValid = false;
+		Person person = new Person();
 
 		
-		while (!isValid) {
+		while (!person.getIsValid()) {
 			Scanner input = new Scanner(System.in); // create input scanner
-			try {
 				
-				System.out.println("Please enter your age: ");
-				myAge = input.nextInt();
-				
-				System.out.println("\nYou entered: " + myAge);
-				isValid = true;
-				
-			} catch (Exception exception) {
-				System.out.println("Please enter a positive number");
+			System.out.println("Please enter your age: ");
+			person.setAge(input.nextLine()); 
+			
+			if(person.getIsValid()) {
+				System.out.println(person.toString());
 			}
+			else {
+				System.out.println();
+			}	
 		}
 		
 		
