7d63c1867f2cf36491bee7213385dca6600a0245
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-09-29 19:45:27 -0400

    added simple menu

--- src/Program.java
+++ src/Program.java
@@ -6,8 +6,33 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Person person = new Person();
-
+		String prompt = "";
 		
+		
+		do {
+			System.out.println("Please Make a Selection: ");
+			System.out.println("-------------------------");
+			System.out.println("1. Enter Your Age");
+			System.out.println("2. Exit");
+			System.out.println("-------------------------");
+			
+			Scanner input = new Scanner(System.in);
+			prompt = input.nextLine();
+			
+			switch (prompt) {
+			case "1":
+				SetAge(person);
+				break;
+			case "2":
+				break;
+			}
+			
+		} while (!prompt.equals("2"));
+		
+	}
+
+	// set person's age
+	private static void SetAge(Person person) {
 		while (!person.getIsValid()) {
 			Scanner input = new Scanner(System.in); // create input scanner
 				
@@ -21,11 +46,7 @@ public class Program {
 				System.out.println();
 			}	
 		}
-		
-		
-		
-		
-		
 	}
+	
 
 }
